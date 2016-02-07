dca5a44e8c2e9e21f638ed5e8f328702c06368a5
Derek Widmayer <dwidmaye@gmail.com>
2016-02-07 20:34:54 +0000

    added flash messaging and bootstrap styles

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,17 +5,19 @@ class ContactsController < ApplicationController
     end
     
     def create
-        @contact = Contact.new(contact_params)
-        
-        if @contact.save
-            redirect_to new_contact_path, notice: "Message sent."
-        else
-           redirect_to new_contact_path, notice: "Error occurred."
-        end
+      @contact = Contact.new(contact_params)
+      
+      if @contact.save
+        flash[:success] = 'Message sent.'
+        redirect_to new_contact_path
+      else
+        flash[:danger] = 'Error occurred, message has not been sent.'
+        redirect_to new_contact_path
+      end
     end
     
     private
-        def contact_params
-            params.require(:contact).permit(:name, :email, :comments)
-        end
+      def contact_params
+          params.require(:contact).permit(:name, :email, :comments)
+      end
 end
