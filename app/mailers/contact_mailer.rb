81c925f7434edf28335afe617c0a66c7238438fb
Derek Widmayer <dwidmaye@gmail.com>
2016-02-14 18:28:44 +0000

    fixed typo in ContactMailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,7 +2,7 @@ class ContactMailer < ActionMailer::Base
     
     default to: 'dwidmaye@gmail.com'
    
-   def contact_email(name, email body)
+   def contact_email(name, email, body)
        @name = name
        @email = email
        @body = body
